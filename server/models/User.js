90c31cfb525ff939a8087ebfe77891969f93b535
OmKadam-1 <kom110798@gmail.com>
2026-02-16 18:28:43 +0530

    user schema added

--- server/models/User.js
+++ server/models/User.js
@@ -1,1 +1,20 @@
-console.log()
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ["ADMIN", "PATIENT"],
+    default: "PATIENT"
+  }
+}, { timestamps: true });
+
+export default mongoose.model("User", userSchema);
