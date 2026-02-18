960f963c314134e9b96c589726d87050d445e8e9
snehalkhater <snehalkhater8@gmail.com>
2026-02-18 19:46:12 +0530

    doctors added

--- client/src/components/TeamSection.jsx
+++ client/src/components/TeamSection.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
 
+const doctors = [
+    {
+        id: 1,
+        name: "Dr. Aarav Sharma",
+        speciality: "Neurologist",
+        experience: "3 years Experience",
+        image: "https://picsum.photos/400/300?random=1",
+    },
+    {
+        id: 2,
+        name: "Dr. Vivaan Patel",
+        speciality: "Cardiologist",
+        experience: "12 years Experience",
+        image: "https://picsum.photos/400/300?random=2",
+    },
+    {
+        id: 3,
+        name: "Dr. Aditya Verma",
+        speciality: "Dermatologist",
+        experience: "5 years Experience",
+        image: "https://picsum.photos/400/300?random=3",
+    },
+    {
+        id: 4,
+        name: "Dr. Rohan Mehta",
+        speciality: "General Physician",
+    experience: "35 years Experience",
+    image: "https://picsum.photos/400/300?random=4",
+  },
+];
 function TeamSection() {
-  return (
-    <div>TeamSection</div>
+return (
+    <div>
+
+    </div>
   )
 }
 
