855082c4d5a4ec067165f642fde12d34884d1315
snehalkhater <snehalkhater8@gmail.com>
2026-02-19 06:19:03 +0530

    doctors config added

--- client/src/components/TeamSection.jsx
+++ client/src/components/TeamSection.jsx
@@ -1,42 +1,7 @@
 import React from "react";
 import Button from "./Button";
 import { MedalIcon } from "lucide-react";
-import doctor1 from "./../assets/doctor1.jpg"
-import doctor2 from "./../assets/doctor2.jpg"
-import doctor3 from "./../assets/doctor3.jpg"
-import doctor4 from "./../assets/doctor4.jpg"
-
-const doctors = [
-    {
-        id: 1,
-        name: "Dr. Aarav Sharma",
-        speciality: "Neurologist",
-        experience: "3 years Experience",
-        image: doctor1,
-    },
-    {
-        id: 2,
-        name: "Dr. Srushti Patel",
-        speciality: "Cardiologist",
-        experience: "12 years Experience",
-        image: doctor2,
-    },
-    {
-        id: 3,
-        name: "Dr. Riddhi Verma",
-        speciality: "Dermatologist",
-        experience: "5 years Experience",
-        image: doctor3,
-    },
-    {
-        id: 4,
-        name: "Dr. Rohan Mehta",
-        speciality: "General Physician",
-        experience: "35 years Experience",
-        image: doctor4,
-    },
-];
-
+import doctors from "../configs/Doctors";
 const TeamSection = () => {
     return (
         <section className="py-20 bg-gradient-to-r from-slate-100 to-teal-50">
