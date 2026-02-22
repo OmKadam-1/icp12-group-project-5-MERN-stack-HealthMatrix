c7f7bd4e09f4c83ab4d280989ad95bf69a4fb232
OmKadam-1 <kom110798@gmail.com>
2026-02-22 12:59:38 +0530

    style to signup

--- client/src/views/auth/Signup.jsx
+++ client/src/views/auth/Signup.jsx
@@ -3,8 +3,6 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import toast, { Toaster } from "react-hot-toast";
 import { setPageTitle } from "../../utils.jsx";
-import Input from "../../components/Input";
-import Button from "../../components/Button";
 
 function Signup() {
   const [form, setForm] = useState({
@@ -27,59 +25,76 @@ function Signup() {
     try {
       await axios.post("http://localhost:8080/api/auth/register", form);
 
-      toast.success("Registration Successful ✅");
+      toast.success("Registration Successful ");
 
-     
       setTimeout(() => {
         navigate("/login");
       }, 1500);
     } catch (error) {
-      toast.error(error.response?.data?.error || "Registration Failed ❌");
+      toast.error(error.response?.data?.error || "Registration Failed ");
     }
   };
 
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-      <Toaster position="top-right" />
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r grey-[500]">
 
-      <div className="bg-white p-6 rounded shadow-md w-80">
-        <h2 className="text-xl font-bold mb-4 text-center">📝 Sign Up</h2>
-        <Input
+      <Toaster/>
+
+     
+      <div className="bg-white/70 backdrop-blur-md shadow-2xl rounded-3xl p-10 w-[400px] text-center">
+
+       
+        <h1 className="text-3xl font-bold text-green-700 mb-2">
+          Patient Registration
+        </h1>
+        <p className="text-gray-600 mb-6">
+          Create your account to book appointments
+        </p>
+
+       
+        <input
           type="text"
-          placeholder="Enter Your Name"
           name="name"
+          placeholder="Enter Your Name"
           value={form.name}
           onChange={handleChange}
+          className="w-full mb-4 px-5 py-3 rounded-full border border-green-300 focus:outline-none focus:ring-2 focus:ring-green-400"
         />
 
-        <Input
+        
+        <input
           type="email"
-          placeholder="Enter Your Email"
           name="email"
+          placeholder="Enter Your Email"
           value={form.email}
           onChange={handleChange}
+          className="w-full mb-4 px-5 py-3 rounded-full border border-green-300 focus:outline-none focus:ring-2 focus:ring-green-400"
         />
 
-        <Input
+       
+        <input
           type="password"
-          placeholder="Enter Your Password"
           name="password"
+          placeholder="Enter Your Password"
           value={form.password}
           onChange={handleChange}
+          className="w-full mb-6 px-5 py-3 rounded-full border border-green-300 focus:outline-none focus:ring-2 focus:ring-green-400"
         />
 
-        <Button
-          title="   Sign Up"
-          size="medium"
-          variant="primary"
+        
+        <button
           onClick={handleSubmit}
-        />
+          className="w-full cursor-pointer bg-green-600 text-white py-3 rounded-full font-semibold hover:bg-green-700 transition duration-300"
+        >
+          Sign Up
+        </button>
 
-        <p className="text-sm text-center mt-3">
+       
+        <p className="text-sm mt-4">
           Already have an account?{" "}
           <span
-            className="text-blue-600 cursor-pointer"
             onClick={() => navigate("/login")}
+            className="text-green-700 font-medium cursor-pointer hover:underline"
           >
             Login
           </span>
